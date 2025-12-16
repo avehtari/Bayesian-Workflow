1157f54370260cfb252f71798741d35f33763dd5
jgabry <jgabry@gmail.com>
2025-12-16 13:50:46 -0700

    Start cleaning up park_rule

--- park_rule/park_rule.R
+++ park_rule/park_rule.R
@@ -2,7 +2,9 @@ library("arm")
 library("posterior")
 library("rstanarm")
 library("cmdstanr")
+library("lme4")
 
+# Prepare data
 park <- read.csv("data/park.csv")
 N <- nrow(park)
 respondents <- sort(unique(park$submission_id))
@@ -23,24 +25,33 @@ male_name <- park$male
 white_name <- park$white
 n_responses_full <- n_responses[respondent]
 
-
+# Fit lme4 model
 data_matrix <- data.frame(y, respondent, item, male_name, white_name, n_responses_full)
-fit_lme4 <- glmer(y ~ (1 | item) + (1 | respondent) + male_name + white_name + n_responses_full, family=binomial(link="logit"), data=data_matrix)
+fit_lme4 <- glmer(
+  y ~ (1 | item) + (1 | respondent) + male_name + white_name + n_responses_full,
+  family = binomial(link = "logit"),
+  data = data_matrix
+)
 display(fit_lme4)
 
+# Refit the model
 n_skipped <- K - n_responses
-
 n_skipped_full <- n_skipped[respondent]
 data_matrix <- data.frame(y, respondent, item, male_name, white_name, n_skipped_full)
-fit_lme4 <- glmer(y ~ (1 | item) + (1 | respondent) + male_name + white_name + n_skipped_full, family=binomial(link="logit"), data=data_matrix)
+fit_lme4 <- glmer(
+  y ~ (1 | item) + (1 | respondent) + male_name + white_name + n_skipped_full,
+  family = binomial(link = "logit"),
+  data = data_matrix
+)
 display(fit_lme4)
 
 mean(y)
-mean(y[male_name==0 & white_name==0 & n_skipped_full==0])
-sum(male_name==0 & white_name==0 & n_skipped_full==0)
+mean(y[male_name == 0 & white_name == 0 & n_skipped_full == 0])
+sum(male_name == 0 & white_name == 0 & n_skipped_full == 0)
 invlogit(-2.42 + 0.07*mean(male_name) + 0.08*mean(white_name) + 0.03*mean(n_skipped_full))
-mean(predict(fit_lme4, type="response"))
+mean(predict(fit_lme4, type = "response"))
 
+# Simulate data and refit
 set.seed(123)
 a_respondent_sim <- rnorm(J, 0, sqrt(VarCorr(fit_lme4)$respondent))
 a_item_sim <- rnorm(K, 0, sqrt(VarCorr(fit_lme4)$item))
@@ -51,7 +62,11 @@ p_sim <- invlogit(a_respondent_sim[respondent] + a_item_sim[item] + X %*% b_sim)
 y_sim <- rbinom(N, 1, p_sim)
 
 data_sim <- data.frame(data_matrix, y_sim)
-fit_lme4_sim <- glmer(y_sim ~ (1 | item) + (1 | respondent) + male_name + white_name + n_responses_full, family=binomial(link="logit"), data=data_sim)
+fit_lme4_sim <- glmer(
+  y_sim ~ (1 | item) + (1 | respondent) + male_name + white_name + n_responses_full,
+  family = binomial(link = "logit"),
+  data = data_sim
+)
 display(fit_lme4_sim)
 
 a_item_hat <- ranef(fit_lme4)$item
@@ -61,9 +76,9 @@ wordings <- read.csv("data/park.txt", header=FALSE)$V2
 wordings <- substr(wordings, 2, nchar(wordings)-1)
 a_item_hat <- unlist(a_item_hat)
 names(a_item_hat) <- wordings
-
 sort(a_item_hat)
 
+# Make plots
 item_avg <- rep(NA, K)
 for (k in 1:K) {
   item_avg[k] <- mean(y[item==k])
@@ -80,64 +95,110 @@ for (j in 1:J) {
 }
 plot(respondent_avg, a_respondent_hat, pch=20, cex=.4)
 
+# Consider order of response
 order_of_response <- rep(NA, N)
 for (j in 1:J) {
   order_of_response[respondent==j] <- 1:n_responses[j]
 }
 data_matrix <- data.frame(data_matrix, order_of_response)
 
-fit_lme4 <- glmer(y ~ (1 | item) + (1 | respondent) + male_name + white_name + n_skipped_full + order_of_response, family=binomial(link="logit"), data=data_matrix)
+fit_lme4 <- glmer(
+  y ~ (1 | item) + (1 | respondent) + male_name + white_name + n_skipped_full + order_of_response,
+  family = binomial(link = "logit"),
+  data = data_matrix
+)
 display(fit_lme4)
 
 # rstanarm
 options(mc.cores = 4)
-
-fit_rstanarm <- stan_glmer(y ~ (1 | item) + (1 | respondent) + male_name + white_name + n_skipped_full + order_of_response, family=binomial(link="logit"), data=data_matrix)
+fit_rstanarm <- stan_glmer(
+  y ~ (1 | item) + (1 | respondent) + male_name + white_name + n_skipped_full + order_of_response,
+  family = binomial(link = "logit"),
+  data = data_matrix
+)
 print(fit_rstanarm)
 
-fit_rstanarm <- stan_glmer(y ~ (1 | item) + (1 | respondent) + male_name + white_name + n_skipped_full + order_of_response, family=binomial(link="logit"), data=data_matrix, iter=200, control=list(max_treedepth=5))
+fit_rstanarm <- stan_glmer(
+  y ~ (1 | item) + (1 | respondent) + male_name + white_name + n_skipped_full + order_of_response, 
+  family = binomial(link = "logit"), data = data_matrix, 
+  iter = 200, control = list(max_treedepth = 5))
 print(fit_rstanarm)
 
-# Stan
-
+# Fit Stan models
 X <- cbind(rep(1,N), male_name, white_name, n_skipped_full, order_of_response)
-stan_data <- list(N=N, J=J, K=K, L=ncol(X), y=y, respondent=respondent, item=item, X=X)
+stan_data <- list(
+  N = N,
+  J = J,
+  K = K,
+  L = ncol(X),
+  y = y,
+  respondent = respondent,
+  item = item,
+  X = X
+)
 
 park_1 <- cmdstan_model("park_1.stan")
-fit_1 <- park_1$sample(data=stan_data, parallel_chains=4, iter_warmup=100, iter_sampling=100, max_treedepth=5)
-print(fit_1)
-
-fit_1 <- park_1$sample(data=stan_data, parallel_chains=4, max_treedepth=5)
+fit_1 <- park_1$sample(
+  data = stan_data,
+  parallel_chains = 4,
+  max_treedepth = 5
+)
 print(fit_1)
 
 park_2 <- cmdstan_model("park_2.stan")
-fit_2 <- park_2$sample(data=stan_data, parallel_chains=4, max_treedepth=5)
-print(fit_2)
-
-fit_2 <- park_2$sample(data=stan_data, parallel_chains=4)
+fit_2 <- park_2$sample(
+  data = stan_data,
+  parallel_chains = 4,
+  max_treedepth = 5
+)
 print(fit_2)
 
 park_3 <- cmdstan_model("park_3.stan")
-fit_3 <- park_3$sample(data=stan_data, parallel_chains=4, max_treedepth=5)
+fit_3 <- park_3$sample(
+  data = stan_data,
+  parallel_chains = 4,
+  max_treedepth = 5
+)
 print(fit_3)
+
 
 # Simulated-data experiment
 stan_data_sim <- stan_data
 stan_data_sim$y <- y_sim
-fit_1_sim <- park_1$sample(data=stan_data_sim, parallel_chains=4, max_treedepth=5)
+fit_1_sim <- park_1$sample(
+  data = stan_data_sim,
+  parallel_chains = 4,
+  max_treedepth = 5
+)
 print(fit_1_sim)
-fit_2_sim <- park_2$sample(data=stan_data_sim, parallel_chains=4, max_treedepth=5)
+
+fit_2_sim <- park_2$sample(
+  data = stan_data_sim,
+  parallel_chains = 4,
+  max_treedepth = 5
+)
 print(fit_2_sim)
-fit_3_sim <- park_3$sample(data=stan_data_sim, parallel_chains=4, max_treedepth=5)
+
+fit_3_sim <- park_3$sample(
+  data = stan_data_sim,
+  parallel_chains = 4,
+  max_treedepth = 5
+)
 print(fit_3_sim)
 
 # Fit normal model
-park_normal <- list(cmdstan_model("park_1_normal.stan"),
-                    cmdstan_model("park_2_normal.stan"),
-                    cmdstan_model("park_3_normal.stan"))
-fit_normal <- as.list(rep(NA, 3))
-for (i in 1:3){
-  fit_normal[[i]] <- (park_normal[[i]])$sample(data=stan_data, parallel_chains=4, max_treedepth=5)
+park_normal <- list(
+  cmdstan_model("park_1_normal.stan"),
+  cmdstan_model("park_2_normal.stan"),
+  cmdstan_model("park_3_normal.stan")
+)
+fit_normal <- list()
+for (i in 1:3) {
+  fit_normal[[i]] <- park_normal[[i]]$sample(
+    data = stan_data,
+    parallel_chains = 4,
+    max_treedepth = 5
+  )
   print(fit_normal[[i]])
 }
 
@@ -150,9 +211,13 @@ sigma_y_sim <- median(sims$sigma_y)
 y_sim <- rnorm(N, a_respondent_sim[respondent] + a_item_sim[item] + X %*% b_sim, sigma_y_sim)
 
 stan_data_sim$y <- y_sim
-fit_normal_sim <- as.list(rep(NA, 3))
-for (i in 1:3){
-  fit_normal_sim[[i]] <- (park_normal[[i]])$sample(data=stan_data_sim, parallel_chains=4, max_treedepth=5)
+fit_normal_sim <- list()
+for (i in 1:3) {
+  fit_normal_sim[[i]] <- park_normal[[i]]$sample(
+    data = stan_data_sim,
+    parallel_chains = 4,
+    max_treedepth = 5
+  )
   print(fit_normal_sim[[i]])
 }
 
---
+++ timeseries/timeseries.R
@@ -0,0 +1,74 @@
+library("scales")
+library("arm")
+library("cmdstanr")
+library("posterior")
+set.seed(123)
+
+series <- matrix(scan("data/Series1000.txt"), nrow=1000, ncol=135, byrow=TRUE)
+T <- 135
+N <- 1000
+
+pdf("series_1.pdf", height=3.4, width=5.5)
+par(mar = c(3, 3, 2, 0), tck = -.01, mgp = c(1.5, .5, 0))
+plot(c(1, T), range(series), 
+     xaxs = "i", bty = "l", type = "n", 
+     xlab = "Time", ylab = "y")
+for (n in 1:N){
+  lines(1:T, series[n,], lwd = .5, col = alpha("black", 0.2))
+}
+dev.off()
+
+slope <- rep(NA, N)
+se <- rep(NA, N)
+for (n in 1:N){
+  data <- series[n,]
+  time <- 1:T
+  fit <- lm(data ~ time)
+  slope[n] <- 100*coef(fit)["time"]
+  se[n] <- 100*se.coef(fit)["time"]
+}
+
+pdf("series_2.pdf", height=3.6, width=5.5)
+par(mar = c(3, 3, 2, 0), tck = -.01, mgp = c(1.5, .5, 0))
+plot(slope, se, 
+     ylim = c(0, 1.05 * max(se)), yaxs = "i", bty = "l", 
+     xlab = "Estmated slope", ylab = "SE", pch = 20, cex = .5)
+dev.off()
+
+pdf("series_3.pdf", height=3.6, width=5.5)
+par(mar = c(3, 3, 2, 0), tck = -.01, mgp = c(1.5, .5, 0))
+hist(
+  slope,
+  xlab = "Estimated slope",
+  breaks = seq(floor(10 * min(slope)), ceiling(10 * max(slope))) / 10,
+  main = ""
+)
+dev.off()
+
+
+y <- slope
+K <- 3
+mu <- c(0, -1, 1)
+data <- list(y = y, K = K, N = N, mu = mu)
+mod <- cmdstan_model("mixture.stan")
+fit_mix <- mod$sample(data = data, parallel_chains = 4, refresh = 0)
+print(fit_mix)
+
+## New model
+mod <- cmdstan_model("mixture_2.stan")
+fit_mix <- mod$sample(data = data, parallel_chains = 4, refresh = 0)
+prob_sims <- as_draws_rvars(fit_mix$draws())
+prob <- mean(prob_sims$p)
+
+max_prob <- apply(prob, 1, max)
+choice <- apply(prob, 1, which.max)
+expected_correct <- sum(max_prob)
+sd_correct <- sqrt(sum(max_prob * (1 - max_prob)))
+print(table(choice))
+pfround(c(expected_correct, sd_correct), 1)
+
+
+pnorm(expected_correct, 900, sd_correct)
+1/pnorm(expected_correct, 900, sd_correct)
+1/pnorm(expected_correct, 899.5, sd_correct)
+
